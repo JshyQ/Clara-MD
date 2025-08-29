4166fbc64e41a49babaf7b5e8c635ef83688b66f
Joshy <145135212+JshyQ@users.noreply.github.com>
2025-08-29 10:58:37 +0700

    Update config.js

--- config.js
+++ config.js
@@ -6,35 +6,35 @@ const readMore = more.repeat(4001)
 
 // Owner
 global.owner = [
-['628xxx', 'Xyroinee', true]
+['6287865843362', 'JoshyQ', true]
 ]
 global.mods = []
 global.prems = []
 // Info
 global.nomorwa = '628xxx'
-global.packname = 'anu - MD by'
-global.author = '© Xyroinee'
-global.namebot = '​anu - MD'
-global.wm = '© anu By Xyroine'
-global.stickpack = 'anu - MD by'
-global.stickauth = '© Xyroine'
+global.packname = 'Joe - MD by'
+global.author = '© Joshy'
+global.namebot = '​Joe - MD'
+global.wm = '© Joe By Joshy'
+global.stickpack = 'Joe - MD by'
+global.stickauth = '© Joshy'
 global.fotonya = 'https://telegra.ph/file/83b522fd80b660705d1cd.jpg'
 // Link Sosmed
 global.sig = 'https://www.instagram.com/xyroinee'
 global.sgh = 'https://github.com/Xyroinee'
 global.sgc = 'https://chat.whatsapp.com/'
 // Donasi
-global.psaweria = 'https://saweria.co/Zeltoria'
-global.ptrakterr = 'https://trakteer.id/zeltoria'
-global.povo = '0896-1075-0607'
+global.psaweria = ''
+global.ptrakterr = ''
+global.povo = ''
 // Info Wait
 global.wait = 'Sedang Di Proses, Mohon Tunggu....'
 global.eror = 'Terjadi Kesalahan Coba Lagi Nanti!'
 global.multiplier = 69 
 // Apikey
-global.lol = 'apikeymu'
-global.rose = 'apikeymu'
-global.xyro = 'apikeymu'
+global.lol = 'b3923d4b'
+global.rose = 'b3923d4b'
+global.xyro = 'b3923d4b'
 // Catatan : Jika Mau Work Fiturnya
 // Masukan Apikeymu
 // Gapunya Apikey? Ya Daftar
@@ -48,7 +48,7 @@ global.APIs = {
 
 /Apikey/
 global.APIKeys = {
-    "https://api.xyroinee.xyz": "apikeymu",
+    "https://api.xyroinee.xyz": "b3923d4b",
 }
 
 let file = fileURLToPath(import.meta.url)
